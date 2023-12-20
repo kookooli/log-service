fa6ccf3ded978e13358a9407551c79f20ed0c25d
Xhino Peci <xhino@kodistat.com>
2023-12-20 12:43:31 +0100

    add swagger and class-validator

--- src/logs/dto/get-logs.input.ts
+++ src/logs/dto/get-logs.input.ts
@@ -1,5 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class GetLogsInput {
+  @ApiProperty({
+    description: 'Provide the name of the file',
+    example: 'test-log-file.log',
+  })
+  @IsNotEmpty()
+  @IsString()
   filename: string;
+
+  @ApiProperty({
+    description: 'Fetch only a limited nr of rows',
+    example: 30,
+  })
+  @IsNumberString()
   nrRows: number;
+
+  @ApiProperty({
+    description: 'Provide a search query',
+    example: 'WARNING',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
   searchQuery?: string;
 }
