9e62107886d02c13fa0ea04b4ecf592e7817e747
Xhino Peci <xhino@kodistat.com>
2024-01-11 17:15:25 +0100

    move to fs/readline

--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -1,50 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { GetLogsInput } from './dto/get-logs.input';
 import * as Exceptions from '@nestjs/common/exceptions';
-import { exec } from 'child_process';
-import * as util from 'util';
 import { ServerConfig } from 'src/config/configs/server.config';
 import { ConfigService } from 'src/config/config.service';
 import * as path from 'path';
+import * as fs from 'fs';
+import * as readline from 'readline';
 
 @Injectable()
 export class LogsService {
   private readonly serverConfig: ServerConfig;
+  private readonly APX_CHUNK_SIZE: number;
 
   constructor(private readonly configService: ConfigService) {
     this.serverConfig = this.configService.getConfig().server;
-  }
-
-  private async executeCommand(command: string): Promise<string[]> {
-    const pExec = util.promisify(exec);
-    try {
-      const { stdout } = await pExec(command, { encoding: 'utf8' });
-
-      return stdout
-        .split('\n')
-        .filter((line) => line.trim() !== '')
-        .reverse();
-    } catch (error) {
-      if (error?.code === 1 && error?.stdout === '' && error?.stderr === '')
-        return ['    ************   NO RESULTS  ************    '];
-      else if (error.stderr.includes('No such file or directory'))
-        throw new Exceptions.NotFoundException('File not found.');
-      else
-        throw new Exceptions.InternalServerErrorException(
-          `Child process error: ${error}`,
-        );
-    }
+    this.APX_CHUNK_SIZE = 1024;
   }
 
   async getLogs(getLogsInput: GetLogsInput): Promise<string[]> {
     const { filename, nrRows, searchQuery } = getLogsInput;
     const filePath = path.join(this.serverConfig.folderLocation, filename);
 
-    let command = `tail -n ${nrRows} ${filePath}`;
-    if (searchQuery) {
-      command += ` | grep '${searchQuery}'`;
-    }
+    const logs: string[] = [];
+    try {
+      const fileSize = fs.statSync(filePath).size;
+      const startPosition = Math.max(
+        0,
+        fileSize - nrRows * this.APX_CHUNK_SIZE,
+      );
 
-    return this.executeCommand(command);
+      return new Promise((resolve, reject) => {
+        const fileStream = fs.createReadStream(filePath, {
+          encoding: 'utf8',
+          start: startPosition,
+        });
+        const readlineOutput = readline.createInterface({
+          input: fileStream,
+          crlfDelay: Infinity,
+        });
+
+        readlineOutput.on('line', (line) => {
+          if (searchQuery && !line.includes(searchQuery)) {
+            return;
+          }
+          logs.unshift(line);
+          if (nrRows && logs.length > nrRows) {
+            logs.pop();
+          }
+        });
+
+        readlineOutput.on('close', () => {
+          resolve(logs);
+        });
+
+        readlineOutput.on('error', (err) => {
+          reject(err);
+        });
+      });
+    } catch (error) {
+      throw new Exceptions.InternalServerErrorException(
+        `Error on reading file: ${error}`,
+      );
+    }
   }
 }
